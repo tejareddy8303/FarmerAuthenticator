ecdc81690539d3fab697fa5a2c96e4663af2751c
pavankumar2410 <pavankumark24102000@gmail.com>
2020-12-30 09:36:29 +0530

    iam trying to commit

---
+++ app/src/main/java/com/example/farmerauthenticator/Reg_bid.java
@@ -0,0 +1,14 @@
+package com.example.farmerauthenticator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Reg_bid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg_bid);
+    }
+}
---
+++ app/src/main/java/com/example/farmerauthenticator/log_bid.java
@@ -0,0 +1,14 @@
+package com.example.farmerauthenticator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class log_bid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_bid);
+    }
+}
